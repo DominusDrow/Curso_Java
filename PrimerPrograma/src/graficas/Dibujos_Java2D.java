856a34fbea82abcd9bdd8a2fc797a0878ddca778
drow <galacticovasquez@gmail.com>
2020-10-12 10:23:40 -0500

    Circulitos UwU

--- PrimerPrograma/src/graficas/Dibujos_Java2D.java
+++ PrimerPrograma/src/graficas/Dibujos_Java2D.java
@@ -1,7 +1,9 @@
 package graficas;
 
 import javax.swing.*;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.geom.Ellipse2D.Double;
 
 public class Dibujos_Java2D {
 
@@ -22,7 +24,7 @@ class MarcoDibujos extends JFrame{
 		
 		setTitle("Dibujitos Java2D");
 		setLocation(200,300);
-		setSize(300,200);		
+		setSize(800,500);		
 		setVisible(true);
 		
 		PanelDibujos panel=new PanelDibujos();
@@ -36,8 +38,37 @@ class PanelDibujos extends JPanel{
 	
 	public void paintComponent(Graphics g) {
 		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D) g;
+		
+		Rectangle2D rectangulo= new Rectangle2D.Double(300,100,200,250);
+		
+		g2.draw(rectangulo);
+		
+		Ellipse2D elipse= new Ellipse2D.Double();
+		
+		elipse.setFrame(rectangulo);
+		
+		g2.draw(elipse);
+		
+		g2.draw(new Line2D.Double(300,100,500,350));
+		
+		Ellipse2D circulo= new Ellipse2D.Double();
+		
+		double centroX= rectangulo.getCenterX();
+		double centroY= rectangulo.getCenterY();
+		
+		circulo.setFrameFromCenter(centroX, centroX, centroX+100, centroY+100);
+		
+		g2.draw(circulo);
 		
 		
 	}
 	
 }
+
+
+
+
+
